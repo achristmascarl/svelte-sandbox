e264ccb1ee90019941ff61c1e110bbd60e0a2f7b
achristmascarl <carl.z.liu@gmail.com>
2021-11-15 00:32:50 -0500

    add shake button, change to 2 space tabs

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,73 +1,82 @@
 <script>
-    import { onMount } from 'svelte';
+  import { onMount } from 'svelte';
 
-    // assume square map
-    let canvas;
-    let ctx;
-    let x = 0;
-    let y = 0;
-    $: drawCursor(x, y);
-    const unit = 5;
-    const ratio = 50;
-    let mapRatio = 1;
+  // assume square map
+  let canvas;
+  let ctx;
+  let x = 0;
+  let y = 0;
+  $: drawCursor(x, y);
+  const unit = 5;
+  const ratio = 50;
+  let mapRatio = 1;
 
-    onMount(() => {
-		ctx = canvas.getContext('2d');
-        mapRatio = canvas.height / (unit * ratio);
+  onMount(() => {
+    ctx = canvas.getContext('2d');
+    mapRatio = canvas.height / (unit * ratio);
 
-        // draw map border
-        ctx.lineWidth = 1;
-        ctx.strokeRect(0, 0, 250, 250);
-        drawCursor(x, y);
-	});
+    // draw map border
+    ctx.lineWidth = 1;
+    ctx.strokeRect(0, 0, 250, 250);
+    drawCursor(x, y);
+  });
 
-    function drawCursor(currentX, currentY) {
-        if (ctx) {
-            console.log(ctx);
-            ctx.fillRect(currentX * mapRatio, currentY * mapRatio, unit * mapRatio, unit * mapRatio);
+  function shakeCanvas() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    // draw map border
+    ctx.lineWidth = 1;
+    ctx.strokeRect(0, 0, 250, 250);
+    drawCursor(x, y);
+  }
+
+  function drawCursor(currentX, currentY) {
+    if (ctx) {
+      console.log(ctx);
+      ctx.fillRect(currentX * mapRatio, currentY * mapRatio, unit * mapRatio, unit * mapRatio);
+    }
+  }
+
+  // watch for movement
+  function handleKeydown(event) {
+    const keyCode = event.keyCode;
+
+    if (keyCode == 65 || keyCode == 37) { // left or a
+        event.preventDefault();
+        if (x - unit >= 0) {
+            x -= unit;
+        } else if (x - unit < 0) {
+            x = 0;
         }
     }
-
-    // watch for movement
-    function handleKeydown(event) {
-        const keyCode = event.keyCode;
-
-        if (keyCode == 65 || keyCode == 37) { // left or a
-            event.preventDefault();
-            if (x - unit >= 0) {
-                x -= unit;
-            } else if (x - unit < 0) {
-                x = 0;
-            }
-        }
-        
-        if (keyCode == 68 || keyCode == 39) { // right or d
-            event.preventDefault();
-            if (x + unit <= unit * ratio) {
-                x += unit;
-            } else if (x + unit > unit * ratio) {
-                x = unit * ratio;
-            }
-        }
-        
-        if (keyCode == 87 || keyCode == 38) { // up or w
-            event.preventDefault();
-            if (y - unit >= 0) {
-                y -= unit;
-            } else if (y - unit < 0) {
-                y = 0;
-            }
-        } 
-        
-        if (keyCode == 83 || keyCode == 40) { // down or s
-            event.preventDefault();
-            if (y + unit <= unit * ratio) {
-                y += unit;
-            } else if (x + unit > unit * ratio) {
-                y = unit * ratio;
-            }
+    
+    if (keyCode == 68 || keyCode == 39) { // right or d
+        event.preventDefault();
+        if (x + unit <= unit * ratio) {
+            x += unit;
+        } else if (x + unit > unit * ratio) {
+            x = unit * ratio;
         }
     }
+    
+    if (keyCode == 87 || keyCode == 38) { // up or w
+        event.preventDefault();
+        if (y - unit >= 0) {
+            y -= unit;
+        } else if (y - unit < 0) {
+            y = 0;
+        }
+    } 
+    
+    if (keyCode == 83 || keyCode == 40) { // down or s
+      event.preventDefault();
+      if (y + unit <= unit * ratio) {
+        y += unit;
+      } else if (x + unit > unit * ratio) {
+        y = unit * ratio;
+      }
+    }
+  }
 </script>
 
 <svelte:window on:keydown={handleKeydown}/>
@@ -75,8 +84,13 @@
 <h1>Welcome to Svelte Sandbox</h1>
 <p>use arrow keys or wasd to move the cursor around</p>
 <canvas
-	bind:this={canvas}
-	width={250}
-	height={250}
+  bind:this={canvas}
+  width={250}
+  height={250}
     class='ml-1'
 ></canvas>
+<button
+  on:click={shakeCanvas}
+>
+  Shake
+</button>
